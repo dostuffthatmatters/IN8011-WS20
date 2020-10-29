072b08e39b02037e08e9aee7ee42c10e9a64c8a7
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-10-29 18:40:55 +0100

    Add code of week 1

---
+++ tutorial-01/example_01_hello_world.c
@@ -0,0 +1,13 @@
+/************************************************************************************
+* Author: Moritz Makowski
+* Lecture: Engineering Informatics 1 (MSE) - WS20
+* Session: Tutorial 01
+************************************************************************************/
+
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ tutorial-01/example_02_hello_math.c
@@ -0,0 +1,16 @@
+/************************************************************************************
+* Author: Moritz Makowski
+* Lecture: Engineering Informatics 1 (MSE) - WS20
+* Session: Tutorial 01
+************************************************************************************/
+
+
+#include <stdio.h>
+
+int main() {
+    int a = 2;
+    int b = 7;
+    int c = a + b;
+    printf("Our number is: %d.\n", c);
+    return 0;
+}
